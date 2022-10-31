c8dfebe42f0117028514c8cd49cff5a7911b374a
Hephezy <oluwapelumimartins5@gmail.com>
2022-10-31 12:09:42 +0100

    Created the profile component

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { RiShareForwardLine } from 'react-icons/ri';
+import Profile from '../../assets/profile_img.png';
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <div className='profile'>
+        <div className='profiles'>
+            <div className='profile-pics'>
+                <img src={Profile} alt='profile-pic' id='profile__img'/>
+                <h1>Oluwapelumi Martins</h1>
+            </div>
+            <div className='profile-share'>
+                <RiShareForwardLine />
+            </div>
+        </div>
+    </div>
   )
 }
 
